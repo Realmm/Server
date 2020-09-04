5cfc7469151cf92d237c4b6ff4156de0d43d80bc
Realmm <30002382+Realmm@users.noreply.github.com>
2020-09-05 06:02:20 +1200

    Added salvaging, beginning work

---
+++ Salvaging/src/main/java/me/realmm/salvaging/Salvaging.java
@@ -0,0 +1,16 @@
+package me.realmm.salvaging;
+
+import net.jamesandrew.realmlib.RealmLib;
+
+public class Salvaging extends RealmLib {
+
+    @Override
+    protected void onStart() {
+
+    }
+
+    @Override
+    protected void onEnd() {
+
+    }
+}
